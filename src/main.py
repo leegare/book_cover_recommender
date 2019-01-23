91f634ca10a15c23f606f3cad57dbc52f24cc0f7
leegare <leegare@hotmail.com>
2019-01-23 14:56:18 -0800

    Correcting path v3

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ warnings.filterwarnings('ignore')
 
 if __name__ == "__main__":
     print("Welcome to book cover recommender.")
-    path = os.getcwd()
+    path = os.getcwd()[:-3]
     print('Path: ',path,'\n',os.listdir())
 
     # Initialize database connection
