00d8f661572fa3eeefb3553e9221e8832158f98c
leegare <leegare@hotmail.com>
2019-01-24 15:47:15 -0800

    added 4 reamining csv files to be loaded

--- src/main.py
+++ src/main.py
@@ -6,14 +6,19 @@ from time import time
 import warnings
 warnings.filterwarnings('ignore')
 
+def ensure_directory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
 if __name__ == "__main__":
     print("Welcome to book cover recommender.")
     path = os.getcwd()[:-3]
-    print('Path: ',path,'\n',os.listdir(path+'data'))
+    print('Path: ',path+'data','\n',os.listdir(path+'data'))
 
     # Initialize database connection
     try:
         # Engine: common interface to the database from SQLAlchemy
+        # Connect to sql_books instance
         engine = create_engine('postgresql://postgres:capstone@35.197.77.78:5432/postgres')
         print("Database found.")
     except:
@@ -21,10 +26,17 @@ if __name__ == "__main__":
         quit()
 
     # Get book listings
-    df = pd.read_csv(path+'/data/external/books.csv', index_col=0)
-    print('Book dataframe size:',df.shape)
+    df = pd.read_csv(path+'data/external/books.csv', index_col=0)
+    df2 = pd.read_csv(path+'data/external/ratings.csv', index_col=0)
+    df3 = pd.read_csv(path+'data/external/to_read.csv', index_col=0)
+    df4 = pd.read_csv(path+'data/external/tags.csv', index_col=0)
+    df5 = pd.read_csv(path+'data/external/book_tags.csv', index_col=0)
 
     insert_db(df, 'books', engine)
+    insert_db(df2, 'ratings', engine)
+    insert_db(df3, 'to_read', engine)
+    insert_db(df4, 'tags', engine)
+    insert_db(df5, 'book_tags', engine)
 
     print('insert_db done')
 
@@ -32,14 +44,19 @@ if __name__ == "__main__":
     data = df.head(10).copy()
 
     # Get cover images
-    # staroot = time()
+
+    # start = time()
 
     os.chdir(path+'data')
-    os.mkdir('covers')
+    try:
+        os.mkdir('covers')
+    except:
+        pass
+    # ensure_directory('./data/covers/')
     os.chdir(path+'data/covers')
 
     data['cover'] = data.apply(lambda row: get_cover(row), axis=1)
-    print('downloading', data.shape[0], 'book covers')
+    print('Downloaded', data.shape[0], 'book covers')
 
     # insert_db(data, 'books_subset', engine)
 
